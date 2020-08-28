158801c69f1367911890ce66ad49e5896342e216
Tomáš Koválik <tomas.kovalik@gmail.com>
2020-08-28 17:03:19 +0200

    even more JAVADOC comments

--- spring/blog/src/main/java/test/blog/controller/CommentController.java
+++ spring/blog/src/main/java/test/blog/controller/CommentController.java
@@ -48,25 +48,39 @@ public class CommentController {
     }
 
     /**
-     * 
-     * @param postId
-     * @return
+     * Get method to retrieve all Comments of the Post.
+     * @param postId - id of a Post for which we want to retrieve Comments
+     * @return list of all Comments
      */
     @GetMapping("/{postId}/comments")
     public List<Comment> findAllByPostID(@PathVariable("postId") Long postId) {
         return this.commentService.findAllByPostID(postId);
     }
 
+    /**
+     * Put method used to update existing Comment.
+     * @param comment - Comment data
+     * @param id - ID of Comment we want to update
+     * @return - updated Comment
+     */
     @PutMapping("/{postId}/comments/{id}")
     public Comment update(@RequestBody Comment comment, @PathVariable("id") Long id) {
         return this.commentService.update(id, comment.getText());
     }
 
+    /**
+     * Delete method used to delete specific Comment.
+     * @param id - ID of Comment we want to delete
+     */
     @DeleteMapping("/{postId}/comments/{id}")
     public void delete(@PathVariable("id") Long id) {
         this.commentService.delete(id);
     }
 
+    /**
+     * Delete method used to delete all Comments for specific Post.
+     * @param postId - ID of Post for which all Comments we want to delete
+     */
     @DeleteMapping("/{postId}/comments")
     public void deleteAll(@PathVariable("postId") Long postId) {
         this.commentService.deleteAllByPostID(postId);
